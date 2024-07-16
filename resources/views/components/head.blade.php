2107c6cc858bf57aa60e49e46e89e33a6f1e309f
Willems Edouard <willems.edouard.pro@gmail.com>
2024-07-16 21:58:32 +0200

    Add head component

---
+++ resources/views/components/head.blade.php
@@ -0,0 +1,16 @@
+@props([
+    'description',
+    'title'
+])
+
+<head {{ $attributes }}>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="{{$description}}">
+
+    <title>{{$title}}</title>
+
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    @vite('resources/css/app.css')
+</head>
