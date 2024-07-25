010a6fdeb77bc78c83ddeb5928a9585ce40157a9
Willems Edouard <willems.edouard.pro@gmail.com>
2024-07-25 16:36:01 +0200

    Add animation on the svg in section-hero

--- resources/views/components/section-hero.blade.php
+++ resources/views/components/section-hero.blade.php
@@ -1,10 +1,11 @@
 <section class=" bg-cover bg-fixed flex justify-center flex-col bg-bottom md:bg-center pt-24 pb-40 bg-hero">
     <div class=' py-16 relative lg:w-full max-w-max lg:max-w-5xl mx-auto h-screen min-h-max  text-center'>
         <h1 class="mb-5 md:px-6 lg:px-0 text-blue-900 text-4xl md:text-5xl lg:text-6xl max-w-sm sm:max-w-[510px] md:max-w-2xl lg:max-w-3xl font-bold mx-auto">Transformer votre vision en réalité <span class="block sm:inline"> digitale</span></h1>
-        <p class='text-lg md:text-2xl lg:text-3xl  text-blue-600 font-medium max-w-4xl md:max-w-full mb-7'>La conception de votre site <span class="block sm:inline"> web sur mesure</span></p>
-        <button class="bg-blue-600 hover:bg-blue-700 hover:scale-105 duration-100 text-white py-4 px-5 rounded-full mx-auto md:mx-0 ">
+        <p class='text-lg md:text-2xl lg:text-3xl  text-blue-600 font-medium max-w-4xl md:max-w-full mb-10'>La conception de votre site <span class="block sm:inline"> web sur mesure</span></p>
+        <a  href="/contact" class=" bg-blue-600 hover:bg-blue-700 block w-max hover:scale-105 focus:scale-105 focus:bg-blue-700 duration-100 text-white py-4 px-5 rounded-full mx-auto">
+
             Discutons de votre projet !
-        </button>
+        </a>
         <x-codesvg class="absolute first-bounce top-60 sm:right-10 lg:top-64 lg:right-44 sm:top-52 sm:w-12 sm:h-12 md:right-20 md:top-56 right-5"/>
             <x-websvg class="absolute second-bounce top-36  sm:h-10 sm:w-10 sm:right-4 sm:top-28 md:top-32 md:right-8 right-5 lg:right-32"/>
                 <x-mailsvg class="absolute second-bounce  top-7 sm:left-6 md:top-20 md:left-0 lg:left-20"/>
