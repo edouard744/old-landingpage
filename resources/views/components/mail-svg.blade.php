e93fa44e34e153a43f71442cf6ad60fc6b7d0da0
Willems Edouard <willems.edouard.pro@gmail.com>
2024-08-06 10:51:30 +0200

    forget to add renamed files

---
+++ resources/views/components/mail-svg.blade.php
@@ -0,0 +1,17 @@
+<svg {{ $attributes->merge(['class' => '']) }} width="38" height="35" viewBox="0 0 38 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d_581_720)">
+<path d="M25.8745 2.54483L31.4169 13.3586C31.6269 13.7683 31.6655 14.2447 31.5243 14.6829C31.3831 15.1211 31.0736 15.4852 30.6638 15.6952L13.6708 24.4047C13.2611 24.6147 12.7847 24.6533 12.3465 24.5121C11.9083 24.3709 11.5442 24.0614 11.3342 23.6516L5.79177 12.8379M25.8745 2.54483C25.6645 2.13511 25.3003 1.8256 24.8622 1.68437C24.424 1.54315 23.9476 1.58178 23.5379 1.79177L6.54482 10.5013C6.13511 10.7113 5.8256 11.0754 5.68437 11.5136C5.54315 11.9518 5.58178 12.4282 5.79177 12.8379M25.8745 2.54483L19.9632 13.5839C19.7965 13.895 19.5389 14.1479 19.2248 14.3089C18.9107 14.4698 18.555 14.5313 18.2051 14.4849L5.79177 12.8379" stroke="#4148E6" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" shape-rendering="crispEdges"/>
+</g>
+<defs>
+<filter id="filter0_d_581_720" x="0.100586" y="0.100683" width="37.0078" height="33.9951" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_581_720"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_581_720" result="shape"/>
+</filter>
+</defs>
+</svg>
